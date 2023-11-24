aae26ffff3f8f0deccb3e45be60424ad583703c7
Neiser omar <49031435+neiseromar11@users.noreply.github.com>
2023-11-24 01:14:37 -0500

    Agregar archivos de proyecto.

---
+++ GuiaSem14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiaSem14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = Pantallas.MenuPrincipal();
+            do
+            {
+                Console.Clear();
+                switch (opcion)
+                {
+                    case 1:
+                        opcion = Pantallas.Vacunacion();
+                        break;
+                    case 2:
+                        opcion = Pantallas.DatosEncuesta();
+                        break;
+                    case 3:
+                        opcion = Pantallas.Resultados();
+                        break;
+                    case 4:
+                        opcion = Pantallas.Buscador();
+                        break;
+                    case 0:
+                    default:
+                        opcion = Pantallas.MenuPrincipal();
+                        break;
+                }
+            } while (opcion != 5);
+            Console.ReadLine();
+        }
+    }
+}
